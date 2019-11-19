1ac7456f3e5422fc38e6d8d40f83731314747b29
Sanskriti Lamsal <slamsal@go.olemiss.edu>
2019-11-18 20:01:57 -0600

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './css/app.css';
+import logo from './assets/logo.png' ;
+import coffee from './assets/coffee.png';
+import searchicon from './assets/search.png';
+import contact from './assets/contact.png';
+class App extends Component{
+  render(){
+    return(
+    <nav class="white">
+      <div class="nav-wrapper">
+        <img src={logo} width="100" height="80" class="brand-logo"/>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+            
+              <li class="navbar-icon">
+                <a href="ddd.html">
+                  <img src={coffee} width="50" height="50"/>
+                </a>
+              </li>
+              <li class="contact-icon navbar-icon" >
+                <a href="bages.html">
+                  <img src={contact} width="50" height="50" />
+                </a>
+              </li>
+             
+        </ul>
+      </div>
+  </nav>
+    )
+  }
 }
+
 
 export default App;
